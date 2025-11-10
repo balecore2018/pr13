d6ae01bb8889a8fa438cb3cb4dbfd7375846d9ae
student-a502 <student-a502@STUD-RM19-A502>
2025-11-10 14:25:25 +0500

    Реализованные интерфейсы. Шаповалов

---
+++ 13prak/Interfaces/IMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13prak.Interfaces
+{
+    public interface IMessages
+    {
+        void All(out List<Messages> messages);
+        void Save(bool Update =  false);
+        void Delete();
+    }
+}
---
+++ 13prak/Interfaces/IUsers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13prak.Interfaces
+{
+    public interface IUsers
+    {
+        void All(out List<Users> Users);
+    }
+}
