4e812807167d3d74d00c24d4ce6245a277b32047
student-a502 <student-a502@STUD-RM19-A502>
2025-11-10 16:05:26 +0500

    Реализованная практическая работа на оценку удовлетворительно. Шаповалов

--- 13prak/MainWindow.xaml.cs
+++ 13prak/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using _13prak.Classes;
 
 namespace _13prak
 {
@@ -20,9 +21,40 @@ namespace _13prak
     /// </summary>
     public partial class MainWindow : Window
     {
+        public UsersContext usersContext = new Classes.UsersContext();
+        public MessagesContext messagesContext = new Classes.MessagesContext();
+        public int IdSelectUser = -1;
+        public static MainWindow mainWindow;
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
+            LoadUser();
+        }
+        public void LoadUser()
+        {
+            foreach(Models.Users User in usersContext.AllUsers)
+                ParentUser.Children.Add(new Elements.Users(User));
+        }
+        public void SelectUser(Models.Users User)
+        {
+            if (User != null)
+                IdSelectUser = usersContext.AllUsers.FindIndex(x => x == User);
+            parentMessage.Children.Clear();
+            foreach (MessagesContext Messages in MessagesContext.AllMessages.FindAll(x=>x.IdUser == IdSelectUser))
+                parentMessage.Children.Add(new Elements.Messages(Messages));
+            BlockMessage.IsEnabled = true;
+        }
+        private void SendMessages(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(e.Key == System.Windows.Input.Key.Enter)
+            {
+                if (IdSelectUser == -1) return;
+                MessagesContext newMessages = new MessagesContext(Message.Text, DateTime.Now, IdSelectUser);
+                newMessages.Save();
+                Message.Text = "";
+                SelectUser(null);
+            }
         }
     }
 }
