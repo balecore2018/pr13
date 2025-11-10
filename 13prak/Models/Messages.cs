d9535eacc86f1008a786304285e0d1d6868a01f5
student-a502 <student-a502@STUD-RM19-A502>
2025-11-10 14:21:54 +0500

    Реализованные модели. Шаповалов

---
+++ 13prak/Models/Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13prak.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Create {  get; set; }
+        public int IdUser { get; set; }
+        public Messages() { }
+        public Messages(string Message, DateTime Create, int IdUser)
+        {
+            this.Message = Message;
+            this.Create = Create;
+            this.IdUser = IdUser;
+        }
+    }
+}
---
+++ 13prak/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13prak.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public Users() { }
+        public Users(int Id, string FIO)
+        {
+            this.Id = Id;
+            this.FIO = FIO;
+        }
+    }
+}
